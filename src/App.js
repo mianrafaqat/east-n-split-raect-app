aa584ae352f2f721a9681ea3328c848b50c595d4
mianrafaqat <rafaqatmehar007@gmail.com>
2023-09-14 19:13:23 +0500

    split form control

--- src/App.js
+++ src/App.js
@@ -66,8 +66,6 @@ const Button = ({ children, onClick }) => {
 };
 
 const FriendList = ({ friends, onSelection, selectedFriend }) => {
-  // console.log(selectedFriend.id);
-  // console.log(friends);
   return (
     <ul>
       {friends.map((friend) => (
@@ -146,21 +144,41 @@ const FormAddFreind = ({ onAddFriend }) => {
 };
 
 const FromSplitBill = ({ selectedFriend }) => {
+  const [bill, setBill] = useState("");
+  const [paidByUser, setPaidByUser] = useState("");
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+  const paidByFriend = bill ? bill - paidByUser : "";
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
-      <label>🕴️Your expense</label>
-      <input type="text" />
+      <label>🕴️ Your expense</label>
+      <input
+        type="text"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
 
       <label>👯‍♀️ {selectedFriend.name}'s expense</label>
-      <input type="text" disabled />
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 Who is patying the bill ?</label>
-      <select>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
